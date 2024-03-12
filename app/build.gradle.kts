ac54c1262021183996d97eb8e28117e26070272b
martin-ngigi <martinwainaina001@gmail.com>
2024-03-12 06:59:13 +0300

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,18 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    /**
+     * Martin added
+     */
+    id("kotlin-kapt")
+//    id("com.google.devtools.ksp")
+
+    id("dagger.hilt.android.plugin")
+//    id("androidx.navigation.safeargs.kotlin")
+    id("androidx.navigation.safeargs")
+//    id("kotlin-android-extensions")
+    id("com.google.gms.google-services") version "4.3.14"
 }
 
 android {
@@ -33,6 +45,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    /**
+     * MARTIN ADDED THIS
+     */
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,9 +64,78 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //retrofit2
+    val retrofit_version = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+    implementation("com.google.code.gson:gson:2.8.9")
+
+    val lifecycle_version = "2.5.1"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+
+    //Swipe refresh
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    // Lottie
+    implementation("com.airbnb.android:lottie:5.0.3")
+
+    //navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
+
+    val coroutines_version = "1.6.1"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
+
+    val roomVersion = "2.4.3"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    val dagger_version = "2.42"
+    implementation("com.google.dagger:hilt-android:$dagger_version")
+    kapt("com.google.dagger:hilt-compiler:$dagger_version")
+
+    val glider_version = "4.13.1"
+    implementation("com.github.bumptech.glide:glide:$glider_version")
+    kapt("com.github.bumptech.glide:compiler:$glider_version")
+
+    // Easy Permission
+    implementation("pub.devrel:easypermissions:3.0.0")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.fragment:fragment-ktx:1.4.1")
+
+    //Lifecycle
+    implementation("androidx.lifecycle:lifecycle-common:2.4.1")
+
+    //size dp/sp
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
+
+    // preferences
+    implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    //CircularImageView
+    implementation("com.github.AtifSayings:CircularImageView:1.0.2")
+
+    //ImagePicker
+    implementation("com.github.dhaval2404:imagepicker:2.1")
+
+    //StepProgress
+    implementation("com.github.funriser:StepProgress:0.1.0")
+
+    //For requesting permissions at runtime
+    implementation("com.karumi:dexter:6.2.2")
+
+
+//    implementation("")
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
